61e0b888d72ef103794a2611b6d4d54eaa421585
Lucas Coelho <llucascoelho33@gmail.com>
2020-09-12 23:18:17 -0300

    Desafio criptografia - strings

--- CRIPTOGRAFIA/criptografia.py
+++ CRIPTOGRAFIA/criptografia.py
@@ -1,14 +1,17 @@
 def main():
 
-    texto = input('Texto: ')
-    chave = int(input('Chave: '))
+    n = int(input())
+
+    for i in range(n):
+        texto = input('Texto: ')
+        chave = int(input('Chave: '))
     
 
-    texto_criptografado = cripto_texto(texto, chave)
-    texto_invertido = inverso_texto(texto, chave)
+        texto_criptografado = cripto_texto(texto, chave)
+        texto_invertido = inverso_texto(texto, chave)
 
-    print(f'{texto_criptografado}')
-    print(f'{texto_invertido}')
+        print(f'{texto_criptografado}')
+        print(f'{texto_invertido}')
 
 
 def cripto_texto(texto, chave):
